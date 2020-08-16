5c72f1b279f995cba4be6453506729784b9ff321
Elvis Lumnica <58716184+elumnica@users.noreply.github.com>
2020-08-15 22:48:40 -0400

    prompt + initial files

---
+++ smallestDiff.js
@@ -0,0 +1,11 @@
+/*
+Propmpt:
+Write a function that takes in two non empty arrays of integers,
+finds the pair of numbers (one from each array) whose absolute difference is closest to zero,
+and returns an array containing these two numbers, with the number from the first array in the 
+first position
+*/
+
+function smallestDifference(arrayOne, arrayTwo){
+    
+}
